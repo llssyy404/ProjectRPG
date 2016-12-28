c974627c6b82bfbeea70d1c444df111eae7ae43d
llssyy404 <christian404@naver.com>
2016-12-28 22:02:16 +0900

    init commit
    Signed-off-by: llssyy404 <christian404@naver.com>

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    // wayPoint값 받아와서 4개 위치 설정
+    // wayPoint값으로 Enemy 위치, Patrol위치 설정
+    // Enemy 인스턴싱
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
