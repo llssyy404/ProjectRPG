20edb9d982a17bb72ede93112fbc671d8eb855b1
Lee Jin-Woo <came1230@naver.com>
2017-01-03 00:48:17 +0900

    scene // ui 동적 할당 테스트

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,7 +6,17 @@ public class GameManager : MonoBehaviour {
     // wayPoint값 받아와서 4개 위치 설정
     // wayPoint값으로 Enemy 위치, Patrol위치 설정
     // Enemy 인스턴싱
-	// Use this for initialization
+    // Use this for initialization
+    void Awake()
+    {
+        GameObject playSceneUIPrefab = Resources.Load("Prefabs/PlaySceneUI") as GameObject;
+        GameObject playScene = GameObject.Instantiate(playSceneUIPrefab) as GameObject;
+
+        GameObject Map01Prefab = Resources.Load("Prefabs/Map01") as GameObject;
+        GameObject Map01 = GameObject.Instantiate(Map01Prefab) as GameObject;
+        Debug.Log("UI and Map Loading Complete");
+    }
+
 	void Start () {
 		
 	}
