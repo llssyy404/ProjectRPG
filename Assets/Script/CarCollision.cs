d03d0a259d04bb4312f7d8f81ec80c4fa4184685
jhoonkime <247347@kpu.ac.kr>
2017-01-09 20:30:29 +0900

    // 조선의 궁궈레 당도한 것을 환영하오 낯선 이여

---
+++ Assets/Script/CarCollision.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollision : MonoBehaviour {
+
+	public float PatrolSpeed = 10.0f;
+
+	//타이머
+	public float f_time;
+
+
+
+	void Start(){
+		//PlayerTrans = GameManager.GetInstance().GetPlayer().transform;
+
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		f_time += Time.deltaTime;
+	}
+//	void OnTriggerExit(Collider col){
+//		if (col.tag == "Car") {
+//			Debug.Log ("asd");
+//
+//			//랜덤돌려서 방향을 정함.
+//			//방향 전환하고 나서 상태를 패트롤로 돌려줌
+//			PatrolSpeed = Random.Range (10, 30);
+//			gameObject.transform.Rotate (0, 180, 0);
+//		}
+//	}
+	void OnTriggerEnter(Collider col){
+		//일단 야매코드;;
+		if (f_time >= 3) {
+			//정석
+			if (col.tag == "Car") {
+				Debug.Log ("asd");
+
+				//랜덤돌려서 방향을 정함.
+				//방향 전환하고 나서 상태를 패트롤로 돌려줌
+				PatrolSpeed = Random.Range (10, 30);
+				gameObject.transform.Rotate (0, 180, 0);
+			}
+		}
+
+	}
+}
